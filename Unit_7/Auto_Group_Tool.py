6363b5a5e664fa241c07194034832eaa451da860
Coen <10960182@uvu.edu>
2024-11-06 23:54:13 -0700

    more work on auto group tool

--- Unit_7/Auto_Group_Tool.py
+++ Unit_7/Auto_Group_Tool.py
@@ -1,6 +1,8 @@
 ﻿import maya.cmds as cmds
 def group_tool():
     sels= cmds.ls(selection=True)
-    objTest=cmds.group(sels, name="objTest")
-    print(objTest)
+    for sel in sels:
+        position=cmds.xform(sel, q=True, ws=True, t=True)
+        rotation=cmds.xform(sel, q=True, ws=True, ro=True)
+        obj_group = cmds.group(sel)
 group_tool()
