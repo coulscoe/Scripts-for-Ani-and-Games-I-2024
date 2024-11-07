4b822b775c0bcaf3dd8c7ec32b3c4ec50bf707ad
Coen <10960182@uvu.edu>
2024-11-07 16:36:49 -0700

    finished auto group tool

--- Unit_7/Auto_Group_Tool.py
+++ Unit_7/Auto_Group_Tool.py
@@ -4,5 +4,8 @@ def group_tool():
     for sel in sels:
         position=cmds.xform(sel, q=True, ws=True, t=True)
         rotation=cmds.xform(sel, q=True, ws=True, ro=True)
-        obj_group = cmds.group(sel)
+        obj_group = cmds.group(sel, empty=True, name=f'{sel}_Grp')
+        cmds.xform(obj_group, ws=True, t=position)
+        cmds.xform(obj_group, ws=True, ro=rotation)
+        parent_obj=cmds.parent(sel, obj_group)
 group_tool()
